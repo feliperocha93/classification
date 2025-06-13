9178ade9ec2a78e402cfb24809700b42cb862456
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-06-13 10:05:23 -0300

    chore: refactor classify

--- llm/classify.js
+++ llm/classify.js
@@ -1,10 +1,5 @@
-import {
-  createPartFromUri,
-  createUserContent,
-  GoogleGenAI,
-  Type,
-} from "@google/genai";
-import fs from "fs";
+import { GoogleGenAI, Type } from "@google/genai";
+import fs, { read } from "fs";
 
 const genai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GENAI_API_KEY,
@@ -12,48 +7,65 @@ const genai = new GoogleGenAI({
 
 const embeddings = JSON.parse(fs.readFileSync("../embeddings.json", "utf8"));
 
-const testEmbeddings = embeddings
+const testPaths = embeddings
   .filter((e) => e.split === "test")
   .map((e) => ".." + e.path.slice(1));
 
-const image = await genai.files.upload({
-  file: testEmbeddings[0],
-  config: {
-    mimeType: "image/jpeg",
-  },
-});
+function readImg(path) {
+  return fs.readFileSync(path, { encoding: "base64" });
+}
 
-const response = await genai.models.generateContent({
-  model: "gemini-2.0-flash",
-  contents: createUserContent([
-    createPartFromUri(image.uri, image.mimeType),
-    `
-You are a classifier that can classify images into one of the following classes: ${[
-      ...new Set(embeddings.map((e) => e.class)),
-    ].join(", ")}.
-Classify the image above and return the class name and the animal color.
-Do not return any other information.
-    `,
-  ]),
-  config: {
-    responseMimeType: "application/json",
-    responseSchema: {
-      type: Type.ARRAY,
-      items: {
-        type: Type.OBJECT,
-        properties: {
-          class: {
-            type: Type.STRING,
-            description: "The predicted class of the image.",
-          },
-          color: {
-            type: Type.STRING,
-            description: "The predicted color of the animal in the image.",
-          },
+const toInlineData = (imgBase64) => {
+  return {
+    inlineData: {
+      mimeType: "image/jpeg",
+      data: imgBase64,
+    },
+  };
+};
+
+const prompt = `
+You are an expert animal classifier. Given an image of an animal,
+classify it into one of the following classes: "cat", "dog".
+Also, provide the primary color of the animal in the image.
+`;
+
+const config = {
+  responseMimeType: "application/json",
+  responseSchema: {
+    type: Type.ARRAY,
+    items: {
+      type: Type.OBJECT,
+      properties: {
+        class: {
+          type: Type.STRING,
+          description: "The predicted class of the image.",
+        },
+        color: {
+          type: Type.STRING,
+          description: "The predicted color of the animal in the image.",
         },
       },
     },
   },
-});
+};
 
-console.log(response.text);
+async function llmRequest(contents) {
+  const response = await genai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: contents,
+    config,
+  });
+
+  return response;
+}
+
+async function llmClassifier(path) {
+  const imgBase64 = readImg(path);
+  const imgInlineData = toInlineData(imgBase64);
+  const contents = [imgInlineData, { text: prompt }];
+  const response = await llmRequest(contents);
+  return await JSON.parse(response.text)[0]["class"];
+}
+
+console.log(await llmClassifier(testPaths[0]));
