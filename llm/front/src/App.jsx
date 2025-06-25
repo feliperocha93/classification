ec8215148bed1bfbb63ad804f2e9272af7b3b2ab
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-06-25 08:32:15 -0300

    chore: front-end style

--- llm/front/src/App.jsx
+++ llm/front/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("");
+  const [imgSrc, setImgSrc] = useState(0);
+
+  function classifyImage() {
+    setImgSrc(inputValue);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Cat or Dog</h1>
+      <input
+        type="text"
+        style={{ width: "100%" }}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={classifyImage}>Cat or Dog</button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={imgSrc}
+          style={{ width: "100%", height: "auto", marginTop: "10px" }}
+        />
+        <span>Caption</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
